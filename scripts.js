a46e6a135d9d5c5f3005753c775f1826c2f94deb
thomassmith <thomassmith@Thomass-MacBook-Pro.local>
2019-11-19 11:37:50 +1100

    added redshift functionality

---
+++ scripts.js
@@ -0,0 +1,77 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+let toggleRed = false;
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+      .then(localMediaStream => {
+        console.log(localMediaStream);
+        video.srcObject = localMediaStream;
+        video.play();
+      })
+      .catch(err => {
+        console.error(`Please allow permission to webcam`, err);
+      });
+  }
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+        if(toggleRed) {
+        // take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // perform red effect on the,
+        pixels = redEffect(pixels);
+        // place the pixels back
+        ctx.putImageData(pixels, 0, 0);
+        }
+       
+    }, 16)
+}
+
+function takePhoto() {
+    // played the sound
+    snap.currentTime = 0;
+    snap.play();
+
+    // take the data out of the canvas
+    const data = canvas.toDataURL('img/jpeg');
+    // console.log(data);
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'download-img')
+    link.innerHTML = `<img src="${data}" alt="download-img"/>`;
+    strip.insertBefore(link, strip.firstChild);
+
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+      pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+      pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
+  }
+
+  function redEffectToggle() {
+    if(toggleRed === false) {
+        toggleRed = true;
+    } else {
+        toggleRed = false;
+    }
+    return toggleRed;
+  }
+
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
